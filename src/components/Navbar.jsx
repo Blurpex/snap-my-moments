ccf310e91c270f8d660e8abcffe18f04adb414a1
Haris Ahmad <harizahmad100@gmail.com>
2024-01-06 00:48:16 -0500

    styled navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,21 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
 
     return(
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/service">Service</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/help">Help</Link>
-            <Link to="/about">About</Link>
-        </div>
+        <nav className="navbar navbar-expand-lg sticky-top bg-white shadow">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    <i className="bi bi-camera2 display-6" />
+                    <span className="display-6">SnapMyMoments</span>
+                </Link>
+                <ul className="navbar-nav">
+                    <Link className="nav-item nav-link" to="/">Home</Link>
+                    <Link className="nav-item nav-link" to="/service">Service</Link>
+                    <Link className="nav-item nav-link" to="/contact">Contact</Link>
+                    <Link className="nav-item nav-link" to="/help">Help</Link>
+                    <Link className="nav-item nav-link" to="/about">About</Link>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
